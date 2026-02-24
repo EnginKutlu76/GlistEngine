0cb2f638257f6fa5d15a26afbc900b8104c99320
EnginKutlu76 <116170963+EnginKutlu76@users.noreply.github.com>
2026-02-24 10:11:18 +0000

    Deploying to gh-pages from @ EnginKutlu76/GlistEngine@36cc2f2779a3c5440ccd16484d42b70c2a9c26af 🚀

---
+++ navtreedata.js
@@ -0,0 +1,88 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "GlistEngine", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classgBaseGUIObject.html#ad8a76f59d9975c919cf8d34a2a0eddc6",
+"classgCameraController.html#a579ffe541faeb1f233a96146ed2ffe4d",
+"classgGLFWWindow.html#a5796a0f73e65428f97cfeff5ca737d52",
+"classgGUIColorbox.html",
+"classgGUIGraph.html#a231a625ebb7bafd1efd2bd0c301a355e",
+"classgGUIMenuItem.html",
+"classgGUIScrollable.html#a512d91ae797ffb13c9a932dbf0e56e6a",
+"classgGUIToolbarButton.html",
+"classgKeyPressedEvent.html#a9f552bf0b15096ab5d37edd5fabd47ec",
+"classgModel.html#a3424d800b0a40000df44b8a486a45f82",
+"classgRect.html#a5d754b00516ec0b7d22c6aa0bed99a59",
+"classgRenderer.html#ab214856baf92df95cfb56ff58641eab6",
+"classgSound.html#a610471dd4d5460d8d5f3f10c4a6b3b89",
+"classgVbo.html#af80337e12ce06252012fddd8c3757a8a",
+"gEvent_8h.html#aa8b0556527d429476e3f3d6cfab1a21faf10ef725671e5ef24acc467c3e3de809",
+"gKeyCode_8h.html#aab88faf49a979dccd4248f462d32a388",
+"gUtils_8h.html#a954551590fa8819f366c7d4d436bd7a2",
+"tinyfiledialogs_8c.html#aeedca8836f3872ca50ebfe723423b25d"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
